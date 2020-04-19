6ef5551791b80413f8190de6997a406d8f8eb4fc
Lucas <lucasluizss@live.com>
2020-04-19 03:00:06 -0300

    ..

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { DatabaseConnection } from './models/database-connection.model';
+declare var UIkit: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'DbCompare';
+
+  public totalComparisons: number;
+  public connectionList = new Array<DatabaseConnection>(0);
+
+  public generateForm(): void {
+    if (!this.totalComparisons) {
+      UIkit.modal.alert(`Total of comparisons must be grather than 2!`);
+      return;
+    }
+
+    for (let i = 0; i < this.totalComparisons; i++) {
+      this.connectionList.push(new DatabaseConnection());
+    }
+  }
+
+  public execute(): void {
+    console.log(this.connectionList);
+    UIkit.modal.alert(`Total of comparisons: ${this.totalComparisons}`);
+  }
+}
---
+++ src/app/models/database-connection.model.ts
@@ -0,0 +1,5 @@
+export class DatabaseConnection {
+	UserName: string;
+	Password: string;
+	Database: string;
+}
