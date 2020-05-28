e1a2a493eea0bd6ae883323834a8939cbdca3b96
Lucas <lucasluizss@live.com>
2020-05-28 11:40:42 -0300

    .

--- application/src/app/services/database-mysql.service.ts
+++ application/src/app/services/database-mysql.service.ts
@@ -2,6 +2,7 @@ import { DatabaseConnection } from './../data/models/database-connection.model';
 import { EDatabaseType } from './../data/enums/database-type.enum';
 import { IDatabaseService } from './interfaces/database-service.interface';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class MySqlService implements IDatabaseService {
   public code: EDatabaseType = EDatabaseType.MySql;
   private responseList: any;
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) { }
 
   public execute(connectionList: DatabaseConnection[]): any {
     console.log(`executing ${this.code.toString()}`);
